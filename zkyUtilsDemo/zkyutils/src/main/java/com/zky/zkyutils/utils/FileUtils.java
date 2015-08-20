6a3bab04037761f1c21d149b7103cbaf90ff8dea
lixiang <long.lixiang@zhongkeyun.com>
2015-08-20 15:33:08 +0800

    增加FileUtils

---
+++ zkyUtilsDemo/zkyutils/src/main/java/com/zky/zkyutils/utils/FileUtils.java
@@ -0,0 +1,27 @@
+package com.zky.zkyutils.utils;
+
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+
+public class FileUtils {
+
+    /**
+     * 获取缓存目录路径
+     *
+     * @param context
+     * @param uniqueName 目录名称
+     * @return
+     */
+    public static File getDiskCacheDir(Context context, String uniqueName) {
+        String cachePath;
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                || !Environment.isExternalStorageRemovable()) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return new File(cachePath + File.separator + uniqueName);
+    }
+}
