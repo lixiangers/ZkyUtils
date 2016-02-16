9d84d4110accb08fbce8eae75c3b15ca88960c7c
lixiangers <lixiangers@163.com>
2016-02-16 17:16:13 +0800

    整理代码

--- zkyUtilsDemo/demo/src/main/java/com/zky/zkyutilsdemo/app/TestSurfaceViewActivity.java
+++ zkyUtilsDemo/demo/src/main/java/com/zky/zkyutilsdemo/app/TestSurfaceViewActivity.java
@@ -3,11 +3,9 @@ package com.zky.zkyutilsdemo.app;
 import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -22,13 +20,12 @@ public class TestSurfaceViewActivity extends Activity {
     Button btnSimpleDraw, btnTimerDraw;
     SurfaceView sfv;
     SurfaceHolder sfh;
-
-    private Timer mTimer;
-    private MyTimerTask mTimerTask;
     int Y_axis[],//保存正弦波的Y轴上的点
             centerY,//中心线
             oldX, oldY,//上一个XY点
             currentX;//当前绘制到的X轴上的点
+    private Timer mTimer;
+    private MyTimerTask mTimerTask;
     private Paint mPaint;
     private Canvas canvas;
 
@@ -65,6 +62,44 @@ public class TestSurfaceViewActivity extends Activity {
         }
     }
 
+    /*
+     * 绘制指定区域
+     */
+    void SimpleDraw(int length) {
+        if (length == 0)
+            oldX = 0;
+        // 关键:获取画布
+        canvas = sfh.lockCanvas(new Rect(oldX, 0, length,
+                getWindowManager().getDefaultDisplay().getHeight()));
+//        Canvas canvas = sfh.lockCanvas(null);// 关键:获取画布
+//        Log.i("Canvas:", oldX + "," + length);
+        if (canvas == null)
+            return;
+
+        int y;
+        for (int i = oldX + 1; i < length; i++) {// 绘画正弦波
+            y = Y_axis[i - 1];
+            canvas.drawLine(oldX, oldY, i, y, mPaint);
+//            Log.i("Canvas:", oldX + "," + oldY + "," + i + "," + y);
+            oldX = i;
+            oldY = y;
+        }
+        sfh.unlockCanvasAndPost(canvas);// 解锁画布，提交画好的图像
+    }
+
+    void ClearDraw() {
+        Canvas canvas = sfh.lockCanvas(null);
+        canvas.drawColor(Color.BLACK);// 清除画布
+        sfh.unlockCanvasAndPost(canvas);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mTimer.cancel();
+    }
+
     class ClickEvent implements View.OnClickListener {
 
         @Override
@@ -95,43 +130,5 @@ public class TestSurfaceViewActivity extends Activity {
             }
         }
 
-    }
-
-    /*
-     * 绘制指定区域
-     */
-    void SimpleDraw(int length) {
-        if (length == 0)
-            oldX = 0;
-        // 关键:获取画布
-        canvas = sfh.lockCanvas(new Rect(oldX, 0, length,
-                getWindowManager().getDefaultDisplay().getHeight()));
-//        Canvas canvas = sfh.lockCanvas(null);// 关键:获取画布
-//        Log.i("Canvas:", oldX + "," + length);
-        if (canvas==null)
-            return;
-
-        int y;
-        for (int i = oldX + 1; i < length; i++) {// 绘画正弦波
-            y = Y_axis[i - 1];
-            canvas.drawLine(oldX, oldY, i, y, mPaint);
-//            Log.i("Canvas:", oldX + "," + oldY + "," + i + "," + y);
-            oldX = i;
-            oldY = y;
-        }
-        sfh.unlockCanvasAndPost(canvas);// 解锁画布，提交画好的图像
-    }
-
-    void ClearDraw() {
-        Canvas canvas = sfh.lockCanvas(null);
-        canvas.drawColor(Color.BLACK);// 清除画布
-        sfh.unlockCanvasAndPost(canvas);
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mTimer.cancel();
     }
 }
