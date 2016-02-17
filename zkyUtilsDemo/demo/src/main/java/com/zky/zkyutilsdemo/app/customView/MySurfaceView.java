3f1188fb22c6b800f506d4242799c4e009c14140
lixiangers <lixiangers@163.com>
2016-02-17 17:51:37 +0800

    完成动态曲线

--- zkyUtilsDemo/demo/src/main/java/com/zky/zkyutilsdemo/app/customView/MySurfaceView.java
+++ zkyUtilsDemo/demo/src/main/java/com/zky/zkyutilsdemo/app/customView/MySurfaceView.java
@@ -5,32 +5,28 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Rect;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class MySurfaceView extends SurfaceView {
     private final Timer mTimer;
     private final MyTimerTask mTimerTask;
     private final Paint mGesturePaint = new Paint();
-    private final Path mPath = new Path();
-    private final Rect mInvalidRect = new Rect();
-    private Context mContex;
+    private int MAX_COUNT;
     private SurfaceHolder sfh;
     private Canvas canvas;
     private int distance = 20;//每个点直接的距离
-    private int oldY;//上一个点的y
-    private int mX;//向左偏移量
-    private boolean isDrawing;
-    private int i;
+    private ArrayBlockingQueue<Integer> pressureValueQueue;
+    private Path path;
 
     public MySurfaceView(Context context) {
         super(context);
-        mContex = context;
         sfh = this.getHolder();
         mGesturePaint.setAntiAlias(true);
         mGesturePaint.setStyle(Paint.Style.STROKE);
@@ -39,14 +35,18 @@ public class MySurfaceView extends SurfaceView {
 
         mTimer = new Timer();
         mTimerTask = new MyTimerTask();
+        path = new Path();
 
         sfh.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
+                MAX_COUNT = getMeasuredWidth() / distance + 1;
+                pressureValueQueue = new ArrayBlockingQueue<Integer>(MAX_COUNT);
+
                 canvas = holder.lockCanvas();
                 canvas.drawColor(Color.BLACK);
                 holder.unlockCanvasAndPost(canvas);
-                mTimer.schedule(mTimerTask, 0, 1000);
+                mTimer.schedule(mTimerTask, 2000, 1000);
             }
 
             @Override
@@ -61,32 +61,46 @@ public class MySurfaceView extends SurfaceView {
         });
     }
 
-    public void drawCanvas(int y) {
+    public void drawCanvas() {
         try {
-            canvas = sfh.lockCanvas(new Rect(0, 0, getWidth(), getHeight()));
-//            canvas = sfh.lockCanvas(null);
-            if (canvas != null) {
-//                canvas.drawPath(mPath, mGesturePaint);
-                canvas.save();
-                mX -= distance;
-                canvas.translate(mX, 0);
-                canvas.drawLine(getWidth() - distance, oldY, getWidth(), y, mGesturePaint);
-                canvas.restore();
-                oldY = y;
-            }
+            canvas = sfh.lockCanvas();
+            path = getPath();
+            canvas.drawColor(Color.BLACK);
+            canvas.drawPath(path, mGesturePaint);
+
         } catch (Exception e) {
-            // TODO: handle exception
         } finally {
             if (canvas != null)
                 sfh.unlockCanvasAndPost(canvas);
         }
     }
 
+    private Path getPath() {
+        path.reset();
+        Integer[] data = new Integer[pressureValueQueue.size()];
+        Integer[] array = pressureValueQueue.toArray(data);
+        int size = array.length;
+        if (size > 0) {
+            path.moveTo(getMeasuredWidth() - (size - 0) * distance, array[0]);
+            Log.d("canvas:", "moveTo:" + "X:" + (getMeasuredWidth() - (size - 0) * distance) + " Y:" + array[0]);
+
+            for (int i = 1; i < size; i++) {
+                path.lineTo(getMeasuredWidth() - (size - i) * distance, array[i]);
+                Log.d("canvas:", "lineTo:" + "X:" + (getMeasuredWidth() - (size - i) * distance) + " Y:" + array[i]);
+            }
+        }
+
+        return path;
+    }
+
     class MyTimerTask extends TimerTask {
         @Override
         public void run() {
-            drawCanvas(new Random().nextInt(200));
-            i++;
+            if (pressureValueQueue.size() == MAX_COUNT)
+                pressureValueQueue.poll();
+            pressureValueQueue.offer(new Random().nextInt(500));
+            drawCanvas();
         }
+
     }
 }
