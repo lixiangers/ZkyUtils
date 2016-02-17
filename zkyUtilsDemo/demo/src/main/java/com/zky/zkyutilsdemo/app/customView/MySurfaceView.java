7f5971905f8ef4c9cefc1d314b7cbfbf91dea28b
lixiangers <lixiangers@163.com>
2016-02-17 11:28:53 +0800

    尝试画动态曲线

--- zkyUtilsDemo/demo/src/main/java/com/zky/zkyutilsdemo/app/customView/MySurfaceView.java
+++ zkyUtilsDemo/demo/src/main/java/com/zky/zkyutilsdemo/app/customView/MySurfaceView.java
@@ -6,25 +6,27 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
-import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MySurfaceView extends SurfaceView {
-    private Context mContex;
-    private float mX;
-    private float mY;
-
-    private SurfaceHolder sfh;
-    private Canvas canvas;
-    private float mCurveEndX;
-    private float mCurveEndY;
-
+    private final Timer mTimer;
+    private final MyTimerTask mTimerTask;
     private final Paint mGesturePaint = new Paint();
     private final Path mPath = new Path();
     private final Rect mInvalidRect = new Rect();
-
+    private Context mContex;
+    private SurfaceHolder sfh;
+    private Canvas canvas;
+    private int distance = 20;//每个点直接的距离
+    private int oldY;//上一个点的y
+    private int mX;//向左偏移量
     private boolean isDrawing;
+    private int i;
 
     public MySurfaceView(Context context) {
         super(context);
@@ -34,15 +36,43 @@ public class MySurfaceView extends SurfaceView {
         mGesturePaint.setStyle(Paint.Style.STROKE);
         mGesturePaint.setStrokeWidth(5);
         mGesturePaint.setColor(Color.WHITE);
-        // TODO Auto-generated constructor stub
+
+        mTimer = new Timer();
+        mTimerTask = new MyTimerTask();
+
+        sfh.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                canvas = holder.lockCanvas();
+                canvas.drawColor(Color.BLACK);
+                holder.unlockCanvasAndPost(canvas);
+                mTimer.schedule(mTimerTask, 0, 1000);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mTimer.cancel();
+            }
+        });
     }
 
-    public void drawCanvas() {
+    public void drawCanvas(int y) {
         try {
-            canvas = sfh.lockCanvas();
+            canvas = sfh.lockCanvas(new Rect(0, 0, getWidth(), getHeight()));
+//            canvas = sfh.lockCanvas(null);
             if (canvas != null) {
-                canvas.drawColor(Color.BLACK);
-                canvas.drawPath(mPath, mGesturePaint);
+//                canvas.drawPath(mPath, mGesturePaint);
+                canvas.save();
+                mX -= distance;
+                canvas.translate(mX, 0);
+                canvas.drawLine(getWidth() - distance, oldY, getWidth(), y, mGesturePaint);
+                canvas.restore();
+                oldY = y;
             }
         } catch (Exception e) {
             // TODO: handle exception
@@ -52,99 +82,11 @@ public class MySurfaceView extends SurfaceView {
         }
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        // TODO Auto-generated method stub
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                touchDown(event);
-//                invalidate();
-                return true;
-
-            case MotionEvent.ACTION_MOVE:
-                if (isDrawing) {
-                    Rect rect = touchMove(event);
-                    if (rect != null) {
-//                        invalidate(rect);
-                    }
-                    return true;
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                if (isDrawing) {
-                    touchUp(event);
-                    invalidate();
-                    return true;
-                }
-                break;
+    class MyTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            drawCanvas(new Random().nextInt(200));
+            i++;
         }
-        return super.onTouchEvent(event);
-    }
-
-    private void touchDown(MotionEvent event) {
-        isDrawing = true;
-        mPath.reset();
-        float x = event.getX();
-        float y = event.getY();
-
-        mX = x;
-        mY = y;
-
-        mPath.moveTo(x, y);
-
-        mInvalidRect.set((int) x, (int) y, (int) x, (int) y);
-        mCurveEndX = x;
-        mCurveEndY = y;
-    }
-
-    private Rect touchMove(MotionEvent event) {
-        Rect areaToRefresh = null;
-
-        final float x = event.getX();
-        final float y = event.getY();
-
-        final float previousX = mX;
-        final float previousY = mY;
-
-        final float dx = Math.abs(x - previousX);
-        final float dy = Math.abs(y - previousY);
-
-        if (dx >= 3 || dy >= 3) {
-            areaToRefresh = mInvalidRect;
-            areaToRefresh.set((int) mCurveEndX, (int) mCurveEndY,
-                    (int) mCurveEndX, (int) mCurveEndY);
-
-            //设置贝塞尔曲线的操作点为起点和终点的一半
-            float cX = mCurveEndX = (x + previousX) / 2;
-            float cY = mCurveEndY = (y + previousY) / 2;
-
-            //实现绘制贝塞尔平滑曲线；previousX, previousY为操作点，cX, cY为终点
-            mPath.quadTo(previousX, previousY, cX, cY);
-            //mPath.lineTo(x, y);
-
-            // union with the control point of the new curve
-            /*areaToRefresh矩形扩大了border(宽和高扩大了两倍border)，
-             * border值由设置手势画笔粗细值决定
-             */
-            areaToRefresh.union((int) previousX, (int) previousY,
-                    (int) previousX, (int) previousY);
-           /* areaToRefresh.union((int) x, (int) y,
-                    (int) x, (int) y);*/
-
-
-            // union with the end point of the new curve
-            areaToRefresh.union((int) cX, (int) cY,
-                    (int) cX, (int) cY);
-
-            //第二次执行时，第一次结束调用的坐标值将作为第二次调用的初始坐标值
-            mX = x;
-            mY = y;
-            drawCanvas();
-        }
-        return areaToRefresh;
-    }
-
-    private void touchUp(MotionEvent event) {
-        isDrawing = false;
     }
 }
