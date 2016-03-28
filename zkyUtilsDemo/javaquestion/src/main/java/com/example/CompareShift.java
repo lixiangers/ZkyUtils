3afad6bacb84009224e428e162d6181303858562
lixiangers <lixiangers@163.com>
2016-03-28 11:41:25 +0800

    添加比较位移和乘除法的速度

--- zkyUtilsDemo/javaquestion/src/main/java/com/example/CompareShift.java
+++ zkyUtilsDemo/javaquestion/src/main/java/com/example/CompareShift.java
@@ -1,14 +1,17 @@
 package com.example;
 
+/**
+ * 发现位移还是快些，但是不是特别的明显
+ */
 public class CompareShift {
     public static void main(String[] args) {
-        int a, b;
+        long a, b;
         long start = System.currentTimeMillis();
         System.out.println("start:" + start);
         long count = 1000000000L;
         for (long i = 0; i < count; i++) {
-            a = 8 / 8;
-            b = 8 * 2;
+            a = i / 8;
+            b = i * 2;
         }
         long end = System.currentTimeMillis();
         System.out.println("end:" + end + "take:" + (end - start));
@@ -16,8 +19,8 @@ public class CompareShift {
         long start2 = System.currentTimeMillis();
         System.out.println("start:" + start2);
         for (long i = 0; i < count; i++) {
-            a = 8 >> 3;
-            b = 8 << 2;
+            a = i >> 3;
+            b = i << 2;
         }
         long end2 = System.currentTimeMillis();
         System.out.println("end:" + end2 + "take:" + (end2 - start2));
